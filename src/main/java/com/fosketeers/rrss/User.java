c8363bb30f7d0db34187c659b8dce03f7e8597c0
furkansimsekli <simseklifurkan0@gmail.com>
2024-04-17 01:27:33 +0300

    backend: add user model
    This is my initial implementation. `type` field is a bit incomplete
    at the moment. I couldn't be sure if I should give the number 4 to
    moderators or add a new boolean field `isModerator`. Since ER table
    shows only `type` (renamed from `flags`, because name flag is ambiguous),
    it remained like that.
    
    I am not sure if we need to define the default values right here, but
    they also can be added later.
    
    Some fields must be mandatory but to keep it simple for now, I didn't go
    into much detail.

---
+++ src/main/java/com/fosketeers/rrss/User.java
@@ -0,0 +1,133 @@
+package com.fosketeers.rrss;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private int type;   // 1: Administrator, 2: Merchant, 3: Customer, ¿Moderator?
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column
+    private LocalDate dateOfBirth;
+
+    @Column
+    private String address;
+
+    @Column
+    private String profileImagePath;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column
+    private String hashedPassword;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getProfileImagePath() {
+        return profileImagePath;
+    }
+
+    public void setProfileImagePath(String profileImagePath) {
+        this.profileImagePath = profileImagePath;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+}
