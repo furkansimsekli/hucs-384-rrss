ef6e85bbae07ba971ea7926a55de7ee3d63e7be1
b2210356097-2 <b2210356097@cs.hacettepe.edu.tr>
2024-04-23 03:08:12 +0300

    [fmt] backend: format code

--- src/main/java/com/fosketeers/rrss/config/DbConfig.java
+++ src/main/java/com/fosketeers/rrss/config/DbConfig.java
@@ -3,16 +3,16 @@ package com.fosketeers.rrss.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("persistence.properties")
 public class DbConfig {
-    @Autowired
-    private Environment env;
+    @Autowired private Environment env;
 
     @Bean
     public DataSource dataSource() {
