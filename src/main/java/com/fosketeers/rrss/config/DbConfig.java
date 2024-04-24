e16badf6b920a3138cb14902840d517cdfe411f6
furkansimsekli <simseklifurkan0@gmail.com>
2024-04-24 23:40:56 +0300

    backend.config.db: use requireNonNull for safety

--- src/main/java/com/fosketeers/rrss/config/DbConfig.java
+++ src/main/java/com/fosketeers/rrss/config/DbConfig.java
@@ -8,6 +8,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
 @PropertySource("persistence.properties")
@@ -17,7 +18,7 @@ public class DbConfig {
     @Bean
     public DataSource dataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("driverClassName"));
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("driverClassName")));
         dataSource.setUrl(env.getProperty("url"));
         dataSource.setUsername(env.getProperty("user"));
         dataSource.setPassword(env.getProperty("password"));
