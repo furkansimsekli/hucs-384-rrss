24e6c5ff4a47d272b78b343fcebf775f7eb1999c
b2210356097-2 <b2210356097@cs.hacettepe.edu.tr>
2024-04-24 13:00:34 +0300

    backend: make user repository queriable by username

--- src/main/java/com/fosketeers/rrss/UserRepository.java
+++ src/main/java/com/fosketeers/rrss/UserRepository.java
@@ -1,8 +1,11 @@
 package com.fosketeers.rrss;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 
-    User findByEmail(String email);
+    Boolean existsByUsername(String username);
 }
