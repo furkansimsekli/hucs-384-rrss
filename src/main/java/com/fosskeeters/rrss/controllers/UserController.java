e3ca20c71f833094cdedde505c3202824b74cc64
İlker Avcı <ikolomiko@gmail.com>
2024-04-26 01:51:19 +0300

    backend,frontend:Impl post handler to update users

--- src/main/java/com/fosskeeters/rrss/controllers/UserController.java
+++ src/main/java/com/fosskeeters/rrss/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.fosskeeters.rrss.controllers;
 
 import com.fosskeeters.rrss.dtos.UserDto;
+import com.fosskeeters.rrss.dtos.UserUpdateDto;
 import com.fosskeeters.rrss.models.User;
 import com.fosskeeters.rrss.repositories.UserRepository;
 
@@ -69,7 +70,62 @@ public class UserController {
         }
 
         // Logged in user is either an admin or the displayed user.
-        model.addAttribute("user", displayedUser.get());
+        UserUpdateDto userUpdateDto = UserUpdateDto.fromUser(displayedUser.get());
+        model.addAttribute("userUpdateDto", userUpdateDto);
+        return "user";
+    }
+
+    @PostMapping("/user/{usernameParam}")
+    public String userUpdateHandler(Model model, @PathVariable String usernameParam, HttpSession session,
+            @Valid @ModelAttribute UserUpdateDto userUpdateDto, BindingResult bindingResult) {
+        Object loggedInUsername = session.getAttribute("username");
+        if (loggedInUsername == null) {
+            return "redirect:/login";
+        }
+
+        Optional<User> loggedInUser = userRepository.findByUsername(loggedInUsername.toString());
+        if (!loggedInUser.isPresent()) {
+            // This condition should never be met but can't be too safe :)
+            return "redirect:/login";
+        }
+
+        Optional<User> displayedUser = userRepository.findByUsername(usernameParam);
+        if (!displayedUser.isPresent()) {
+            model.addAttribute("errorString", "No such user found");
+            return "user";
+        }
+
+        if (loggedInUser.get().getType() != 1 // if user is not an Admin (TODO avoid magic numbers)
+                && !loggedInUser.get().getUsername().equals(displayedUser.get().getUsername())) {
+            // the logged in user and displayed user are different
+            model.addAttribute("errorString", "Unauthorized access");
+            return "user";
+        }
+
+        validateUserUpdate(userUpdateDto, bindingResult, displayedUser.get());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("hasErrors", "true");
+            System.out.println(bindingResult);
+            return "user";
+        }
+
+        User user = new User();
+        user.setId(displayedUser.get().getId());
+        user.setUsername(displayedUser.get().getUsername());
+        user.setPassword(displayedUser.get().getPassword());
+        user.setType(displayedUser.get().getType());
+        user.setProfileImagePath(userUpdateDto.getProfileImagePath());
+        user.setAddress(userUpdateDto.getAddress());
+        user.setDateOfBirth(userUpdateDto.getDateOfBirth());
+        user.setEmail(userUpdateDto.getEmail());
+        user.setFirstName(userUpdateDto.getFirstName());
+        user.setLastName(userUpdateDto.getLastName());
+        user.setPhoneNumber(userUpdateDto.getPhoneNumber());
+        userRepository.save(user);
+
+        //model.addAttribute("userUpdateDbo", user);
+        model.addAttribute("updatedSuccessfully", "true");
         return "user";
     }
 
@@ -155,6 +211,21 @@ public class UserController {
         if (!Objects.equals(userDto.getPassword1(), userDto.getPassword2())) {
             bindingResult.addError(
                     new FieldError("userDto", "password1", "Passwords do not match!"));
+        }
+    }
+
+    private void validateUserUpdate(UserUpdateDto userUpdateDto, BindingResult bindingResult, User oldUser) {
+        if (!oldUser.getEmail().equals(userUpdateDto.getEmail())
+                && userRepository.existsByEmail(userUpdateDto.getEmail())) {
+            bindingResult.addError(
+                    new FieldError("userUpdateDto", "email", "This email address is already taken!"));
+        }
+
+        if (!oldUser.getPhoneNumber().equals(userUpdateDto.getPhoneNumber())
+                && userRepository.existsByPhoneNumber(userUpdateDto.getPhoneNumber())) {
+            bindingResult.addError(
+                    new FieldError(
+                            "userUpdateDto", "phoneNumber", "This phone number is already taken!"));
         }
     }
 
