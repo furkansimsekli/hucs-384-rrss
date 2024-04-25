7bd8484a6a54fa70d333610beb6dfddb34e5a3dc
furkansimsekli <simseklifurkan0@gmail.com>
2024-04-25 22:37:14 +0300

    backend.models: add BrowsingHistory model

---
+++ src/main/java/com/fosskeeters/rrss/models/BrowsingHistory.java
@@ -0,0 +1,55 @@
+package com.fosskeeters.rrss.models;
+
+import jakarta.persistence.*;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+public class BrowsingHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false, cascade = CascadeType.ALL)
+    @JoinColumn
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false, cascade = CascadeType.ALL)
+    @JoinColumn
+    private User owner;
+
+    @CreatedDate LocalDateTime viewedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public LocalDateTime getViewedAt() {
+        return viewedAt;
+    }
+
+    public void setViewedAt(LocalDateTime viewedAt) {
+        this.viewedAt = viewedAt;
+    }
+}
