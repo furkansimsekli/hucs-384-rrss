7f50e8c3d172f1f7dcf091f6eec996913fc35d06
furkansimsekli <simseklifurkan0@gmail.com>
2024-04-25 06:16:32 +0300

    backend.dtos.user: use @NotNull, remove reduntand validation methods
    `type` field was not matching with the one in template, had to change
    it to `accountType`.
    
    Validation has to be done in the controller unfortunately. A solution
    can be implemented by the seeking out the order of binding probably,
    but it doesn't worth the effort.

--- src/main/java/com/fosskeeters/rrss/dtos/UserDto.java
+++ src/main/java/com/fosskeeters/rrss/dtos/UserDto.java
@@ -1,46 +1,40 @@
 package com.fosskeeters.rrss.dtos;
 
-import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class UserDto {
+    @NotNull
     @Size(min = 1, max = 64, message = "First name must be between 1-64 characters!")
     private String firstName;
 
+    @NotNull
     @Size(min = 1, max = 64, message = "Last name must be between 1-64 characters!")
     private String lastName;
 
+    @NotNull
     @Size(min = 1, max = 64, message = "Username must be between 1-16 characters!")
     private String username;
 
+    @NotNull
     @Size(min = 5, max = 160, message = "Email address must be between 5-160 characters!")
     @Email
     private String email;
 
+    @NotNull
     @Size(min = 1, max = 64, message = "Phone number must be between 1-15 characters!")
     private String phoneNumber;
 
-    // FIXME: constrain to hold only "Customer" and "Merchant"
+    @NotNull
     @NotBlank(message = "User type is required!")
-    private String type;
+    private String accountType;
 
+    @NotNull()
     @Size(min = 1, max = 256, message = "Password length must be between 1-256 characters!")
     private String password1;
 
+    @NotNull
     @Size(min = 1, max = 256, message = "Password length must be between 1-256 characters!")
     private String password2;
-
-    @AssertTrue(message = "Passwords do not match!")
-    private boolean doesPasswordMatch() {
-        return password1.equals(password2);
-    }
-
-    @AssertTrue
-    private boolean isTypeValid() {
-        return type.equals("Customer") || type.equals("Merchant");
-    }
 
     public String getFirstName() {
         return firstName;
@@ -82,12 +76,12 @@ public class UserDto {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getType() {
-        return type;
+    public String getAccountType() {
+        return accountType;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setAccountType(String type) {
+        this.accountType = type;
     }
 
     public String getPassword1() {
