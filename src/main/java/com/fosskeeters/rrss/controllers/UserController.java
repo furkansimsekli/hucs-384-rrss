783379b35b75566034ee2618a0c185835577ca65
furkansimsekli <simseklifurkan0@gmail.com>
2024-05-04 08:00:28 +0300

    refactor: use 404 page, validate results of db queries
    Spring Jpa design is so awful that they return a null value if the
    database can't find something. That's why we need to check whether
    the response is null or not everytime we use the repository. I
    looked for if we can somehow customize the repository/jpa so that
    they can throw an exception instead of returning null, but no
    viable result. Therefore I added missing control blocks. While
    doing that, I also made small adjustments on the authorization and
    authentication logic so that there are fewer database query.
    
    Sometimes ORMs are smart, they don't immediately hit the database.
    Maybe that was the case here, I didn't make a research, but still
    there were situtations where we queried for displayedUser twice.

--- src/main/java/com/fosskeeters/rrss/controllers/UserController.java
+++ src/main/java/com/fosskeeters/rrss/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.fosskeeters.rrss.dtos.UserUpdateDto;
 import com.fosskeeters.rrss.models.User;
 import com.fosskeeters.rrss.repositories.UserRepository;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -43,15 +45,20 @@ public class UserController {
     @GetMapping("/{usernameParam}")
     public String userProfileHandler(Model model, @PathVariable String usernameParam,
                                      HttpSession session) {
-        var authorizationRedirect = checkAuthorization(session, model, usernameParam);
+        Optional<User> displayedUser = userRepository.findByUsername(usernameParam);
+
+        if (displayedUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        var authorizationRedirect = checkAuthorization(session, displayedUser.get());
+
         if (authorizationRedirect.isPresent()) {
             return authorizationRedirect.get();
         }
 
-        User displayedUser = userRepository.findByUsername(usernameParam).get();
-
-        // Logged in user is either an admin or the displayed user.
-        UserUpdateDto userUpdateDto = new UserUpdateDto(displayedUser);
+        // Logged-in user is either an admin or the displayed user.
+        UserUpdateDto userUpdateDto = new UserUpdateDto(displayedUser.get());
         model.addAttribute("userUpdateDto", userUpdateDto);
         return "user";
     }
@@ -61,13 +68,19 @@ public class UserController {
                                     HttpSession session,
                                     @Valid @ModelAttribute UserUpdateDto userUpdateDto,
                                     BindingResult bindingResult) {
-        var authorizationRedirect = checkAuthorization(session, model, usernameParam);
+        Optional<User> displayedUser = userRepository.findByUsername(usernameParam);
+
+        if (displayedUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        var authorizationRedirect = checkAuthorization(session, displayedUser.get());
+
         if (authorizationRedirect.isPresent()) {
             return authorizationRedirect.get();
         }
-        User displayedUser = userRepository.findByUsername(usernameParam).get();
 
-        validateUserUpdate(userUpdateDto, bindingResult, displayedUser);
+        validateUserUpdate(userUpdateDto, bindingResult, displayedUser.get());
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("hasErrors", "true");
@@ -75,8 +88,8 @@ public class UserController {
             return "user";
         }
 
-        displayedUser.setFromUserUpdateDto(userUpdateDto);
-        userRepository.save(displayedUser);
+        displayedUser.get().setFromUserUpdateDto(userUpdateDto);
+        userRepository.save(displayedUser.get());
         model.addAttribute("updatedSuccessfully", "true");
         return "user";
     }
@@ -84,7 +97,13 @@ public class UserController {
     @GetMapping("/{usernameParam}/change-password")
     public String getChangePasswordHandler(Model model, @PathVariable String usernameParam,
                                            HttpSession session) {
-        var authorizationRedirect = checkAuthorization(session, model, usernameParam);
+        Optional<User> displayedUser = userRepository.findByUsername(usernameParam);
+
+        if (displayedUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        var authorizationRedirect = checkAuthorization(session, displayedUser.get());
 
         if (authorizationRedirect.isPresent()) {
             return authorizationRedirect.get();
@@ -100,22 +119,27 @@ public class UserController {
                                             HttpSession session,
                                             @Valid @ModelAttribute ChangePasswordDto dto,
                                             BindingResult bindingResult) {
-        var authorizationRedirect = checkAuthorization(session, model, usernameParam);
+        Optional<User> displayedUser = userRepository.findByUsername(usernameParam);
+
+        if (displayedUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        var authorizationRedirect = checkAuthorization(session, displayedUser.get());
 
         if (authorizationRedirect.isPresent()) {
             return authorizationRedirect.get();
         }
 
-        User displayedUser = userRepository.findByUsername(usernameParam).get();
-        validateChangePassword(dto, bindingResult, displayedUser);
+        validateChangePassword(dto, bindingResult, displayedUser.get());
 
         if (bindingResult.hasErrors()) {
             System.out.println(bindingResult);
             return "change_password";
         }
 
-        displayedUser.setPassword(encoder.encode(dto.getNewPassword1()));
-        userRepository.save(displayedUser);
+        displayedUser.get().setPassword(encoder.encode(dto.getNewPassword1()));
+        userRepository.save(displayedUser.get());
         model.addAttribute("updatedSuccessfully", "true");
         return "change_password";
     }
@@ -154,35 +178,26 @@ public class UserController {
             return Optional.of("redirect:/login");
         }
 
-        Optional<User> loggedInUser = userRepository.findByUsername(loggedInUsername.toString());
-        if (loggedInUser.isEmpty()) {
-            // This condition should never be met but can't be too safe :)
-            return Optional.of("redirect:/login");
-        }
-
         return Optional.empty(); // keep going
     }
 
-    private Optional<String> checkAuthorization(HttpSession session, Model model,
-                                                String usernameParam) {
+    private Optional<String> checkAuthorization(HttpSession session, User displayedUser) {
         var authenticationRedirect = checkAuthentication(session);
         if (authenticationRedirect.isPresent()) {
             return authenticationRedirect;
         }
 
-        Optional<User> displayedUser = userRepository.findByUsername(usernameParam);
-        if (displayedUser.isEmpty()) {
-            model.addAttribute("errorString", "No such user found");
-            return Optional.of("user");
+        Optional<User> loggedInUser =
+                userRepository.findByUsername(session.getAttribute("username").toString());
+
+        if (loggedInUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
-        User loggedInUser =
-                userRepository.findByUsername(session.getAttribute("username").toString()).get();
-        if (loggedInUser.getType() != User.Type.ADMIN
-            && !loggedInUser.getUsername().equals(displayedUser.get().getUsername())) {
-            // the logged in user and displayed user are different
-            model.addAttribute("errorString", "Unauthorized access");
-            return Optional.of("user");
+        // Only let the admins and the owner proceed
+        if (loggedInUser.get().getType() != User.Type.ADMIN
+            && !loggedInUser.get().getUsername().equals(displayedUser.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
         return Optional.empty();
