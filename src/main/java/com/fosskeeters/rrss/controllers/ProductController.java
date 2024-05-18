e4c137142995f0a1f172294fa5569152b5a859b8
furkansimsekli <simseklifurkan0@gmail.com>
2024-05-19 01:29:43 +0300

    fix.backend: skip empty files
    SpringBoot weirdly adds an empty file to the list of MultipartFile
    if users doesn't upload any. Therefore I added a control block to
    skip when the file is empty.
    
    ContentType is application/octet-stream for the empty file.
    
    Fixes #119
    
    Here, I realized that we don't do any check whether the uploaded
    files are images. We can do it by checking the ContentType but I'm
    not sure if it would be a certain solution. It doesn't break the
    program, if they upload such file then they can't see the content.
    Therefore I don't see this as priority since there are 5 days left
    for final presentation.

--- src/main/java/com/fosskeeters/rrss/controllers/ProductController.java
+++ src/main/java/com/fosskeeters/rrss/controllers/ProductController.java
@@ -149,6 +149,8 @@ public class ProductController {
             int displayOrder = 1;
 
             for (MultipartFile image : productDto.getImages()) {
+                if (image.isEmpty()) continue;
+
                 String storageFileName = LocalDateTime.now() + "_" + image.getOriginalFilename();
                 String storagePathStr = "/product-images/" + storageFileName;
 
@@ -232,6 +234,8 @@ public class ProductController {
             int displayOrder = 1;
 
             for (MultipartFile image : productDto.getImages()) {
+                if (image.isEmpty()) continue;
+
                 String storageFileName = LocalDateTime.now() + "_" + image.getOriginalFilename();
                 String storagePathStr = "/product-images/" + storageFileName;
 
