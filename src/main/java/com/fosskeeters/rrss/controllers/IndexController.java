52b26cbcc81b7d1e5223b0e446ca5b770db6d2b6
Eyüp Menevşe <menevseeyup@gmail.com>
2024-05-21 14:20:44 +0300

    backend: fix typo

--- src/main/java/com/fosskeeters/rrss/controllers/IndexController.java
+++ src/main/java/com/fosskeeters/rrss/controllers/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
     public String indexGetController(Model model, HttpSession session){
         Optional<List<Product>> latestProducts = productRepository.findTop10ByOrderByCreatedAtDesc();
 
-        if (session.getAttribute("username") == null) {
+        if (!session.getAttribute("username") == null) {
             // TODO : Use recommendation algorithm here.
         }
 
