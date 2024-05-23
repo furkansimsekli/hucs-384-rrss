5bfc47ed86114a9d2b07b091d1cc94cc6dc08b93
İlker Avcı <ikolomiko@gmail.com>
2024-05-24 00:56:22 +0300

    backend: allow anon users to visit merchant page

--- src/main/java/com/fosskeeters/rrss/controllers/MerchantController.java
+++ src/main/java/com/fosskeeters/rrss/controllers/MerchantController.java
@@ -55,29 +55,24 @@ public class MerchantController {
     public String getMerchantProducts(@RequestParam(required = false, defaultValue = "0") int page,
                                       HttpSession session, @PathVariable String username,
                                       Model model) {
-        if (session.getAttribute("username") == null) {
-            return "redirect:/login?next=/merchants/" + username;
-        }
-
         Optional<User> authenticatedUser =
                 userRepository.findByUsername((String) session.getAttribute("username"));
         Optional<User> targetUser = userRepository.findByUsername(username);
-
-        if (authenticatedUser.isEmpty()) {
-            session.removeAttribute("username");
-            return "redirect:/login?next=/merchants/" + username;
-        }
 
         if (targetUser.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
-        if (authenticatedUser.get().getId() == targetUser.get().getId()) {
-            model.addAttribute("isOwner", true);
-        }
+        if (authenticatedUser.isEmpty()) {
+            session.removeAttribute("username");
+        } else { // Visitor is authenticated
+            if (authenticatedUser.get().getId() == targetUser.get().getId()) {
+                model.addAttribute("isOwner", true);
+            }
 
-        if (authenticatedUser.get().getType() == User.Type.ADMIN) {
-            model.addAttribute("isAdmin", true);
+            if (authenticatedUser.get().getType() == User.Type.ADMIN) {
+                model.addAttribute("isAdmin", true);
+            }
         }
 
         if (targetUser.get().getType() != User.Type.MERCHANT) {
